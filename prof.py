09702af0e8c5d112e87514c5aa0af069b6231454
Rishab Mallick <rishabmallick6@gmail.com>
2023-07-13 20:20:40 +0530

    feat: adds basic loop to test tpu use

---
+++ prof.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+import os
+import tensorflow_datasets as tfds
+
+resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='')
+tf.config.experimental_connect_to_cluster(resolver)
+# This is the TPU initialization code that has to be at the beginning.
+tf.tpu.experimental.initialize_tpu_system(resolver)
+print("All devices: ", tf.config.list_logical_devices('TPU'))
+
+# code off https://www.tensorflow.org/guide/tpu for sanity check
+print('Manual device placement')
+a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+
+with tf.device('/TPU:0'):
+  c = tf.matmul(a, b)
+
+print("c device: ", c.device)
+print(c)
+
+print('Distribution strategies')
+strategy = tf.distribute.TPUStrategy(resolver)
+
+@tf.function
+def matmul_fn(x, y):
+  z = tf.matmul(x, y)
+  return z
+
+z = strategy.run(matmul_fn, args=(a, b))
+print(z)
+
+print('Classification on TPUs')
+def create_model():
+  return tf.keras.Sequential(
+      [tf.keras.layers.Conv2D(256, 3, activation='relu', input_shape=(28, 28, 1)),
+       tf.keras.layers.Conv2D(256, 3, activation='relu'),
+       tf.keras.layers.Flatten(),
+       tf.keras.layers.Dense(256, activation='relu'),
+       tf.keras.layers.Dense(128, activation='relu'),
+       tf.keras.layers.Dense(10)])
+  
+def get_dataset(batch_size, is_training=True):
+  split = 'train' if is_training else 'test'
+  dataset, info = tfds.load(name='mnist', split=split, with_info=True,
+                            as_supervised=True, try_gcs=True)
+
+  # Normalize the input data.
+  def scale(image, label):
+    image = tf.cast(image, tf.float32)
+    image /= 255.0
+    return image, label
+
+  dataset = dataset.map(scale)
+
+  # Only shuffle and repeat the dataset in training. The advantage of having an
+  # infinite dataset for training is to avoid the potential last partial batch
+  # in each epoch, so that you don't need to think about scaling the gradients
+  # based on the actual batch size.
+  if is_training:
+    dataset = dataset.shuffle(10000)
+    dataset = dataset.repeat()
+
+  dataset = dataset.batch(batch_size)
+
+  return dataset
+
+with strategy.scope():
+  model = create_model()
+  model.compile(optimizer='adam',
+                # Anything between 2 and `steps_per_epoch` could help here.
+                steps_per_execution = 50,
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['sparse_categorical_accuracy'])
+
+batch_size = 200
+steps_per_epoch = 60000 // batch_size
+validation_steps = 10000 // batch_size
+
+train_dataset = get_dataset(batch_size, is_training=True)
+test_dataset = get_dataset(batch_size, is_training=False)
+
+model.fit(train_dataset,
+          epochs=5,
+          steps_per_epoch=steps_per_epoch,
+          validation_data=test_dataset,
+          validation_steps=validation_steps)
+
+print('Training complete')
